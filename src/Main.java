ce06a3018032d73f9d84cf70562e24502dfbe885
Im-Humor <orangite@gmail.com>
2024-03-21 08:26:27 -0700

    new repository with proper project structure

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.List;
+
+public class Main {
+
+    public static boolean SearchArray(String string, String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equalsIgnoreCase(string)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Hello, and welcome to my note taking Application.");
+        System.out.println("Would you like to create 'New', 'Select', or 'Delete'?");
+
+        String[] optionList = {"new", "select", "delete"};
+
+        String option = scnr.nextLine();
+        while (SearchArray(option, optionList) == false ) {
+            System.out.println("Please try again");
+            option = scnr.nextLine();
+        }
+        System.out.println("That's a valid option!");
+
+
+
+    }
+}
